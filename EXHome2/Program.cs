f8c798e539c5c6803605727330ed26b3fa7b3fb3
ALUNO <ALUNO@LABFPC21>
2024-02-28 10:45:46 -0300

    Adicionar arquivos de projeto.

---
+++ EXHome2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXHome2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio;
+            double pi = 3.141592;
+            raio = double.Parse(Console.ReadLine());
+            double area = (raio * raio) * pi;
+            Console.WriteLine("A=" + String.Format("{0:C4}", area) + "\n");
+            Console.ReadKey();
+        }
+    }
+}
